c7f1e796a598cd4fbfca6519c0c9088259fcd064
Chrystalio <chrystalio.k@outlook.com>
2025-11-16 21:41:39 +0700

    Add GET category by ID route

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 // Route to get all categories
 router.get('/', categoryController.getAllCategories);
+// Route to get a category by ID
+router.get('/:id', categoryController.getCategoryById);
 
 module.exports = router;
